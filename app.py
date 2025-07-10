a89905bbcc5d6d6ecced84e1039c2cbc489480db
evelynyxy <evelyn.yxy@gmail.com>
2025-07-10 14:29:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-import streamlit as st
-import joblib
-import pandas as pd
 
-# load model
-price_model= joblib.load("/Users/gohzhengyong/Desktop/Machine_Learning_Projects/Project1/model/price_model.pkl")
-region_model= joblib.load("/Users/gohzhengyong/Desktop/Machine_Learning_Projects/Project1/model/region_model.pkl")
-
-# slidebar input
-st.sidebar.header("Enter user information")
-location = st.sidebar.selectbox("Location", ['KL', 'Selangor', 'PJ', 'Kajang', 'Sunway'])
-num_bath = st.sidebar.slider("Number of bathrooms", 1,10)
